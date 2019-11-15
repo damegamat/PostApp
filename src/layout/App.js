66db0f7b8c9550e6b8b66009ecb43b2b78c73d2a
Darek <dar.lewandowski93@gmail.com>
2019-11-15 19:56:49 +0100

    add layout components

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import Header from "./Header";
+import Navigation from "./Navigation";
+import Main from "./Main";
+import Footer from "./Footer";
+import { BrowserRouter as Router } from "react-router-dom";
 import "../styles/css/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <header className="Head">
+          <Header />
+        </header>
+        <nav>
+          <Navigation />
+        </nav>
+        <main>
+          <Main />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
