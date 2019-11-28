d95dfb18412d3540749de4cb3713c56aea97a6e5
Darek <dar.lewandowski93@gmail.com>
2019-11-28 23:05:51 +0100

    add Main

---
+++ src/layout/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserData from "../../components/UserData/UserData";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <UserData />
+    </div>
+  );
+};
+
+export default Main;
