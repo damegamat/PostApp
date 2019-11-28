ef42a20d507978ab11a2c45756fc4b2893f5b2db
Darek <dar.lewandowski93@gmail.com>
2019-11-28 23:06:39 +0100

    add Footer

---
+++ src/layout/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <h4>{`Post-App`.toUpperCase()}</h4>
+    </div>
+  );
+};
+
+export default Footer;
