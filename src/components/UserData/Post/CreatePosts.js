b8a3c99647f6913248f5e7fa90c59d1a41bb2816
Darek <dar.lewandowski93@gmail.com>
2020-01-04 20:25:09 +0100

    add mapstatetoprops , connect

--- src/components/UserData/Post/CreatePosts.js
+++ src/components/UserData/Post/CreatePosts.js
@@ -1,35 +1,48 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
 import { Input, Button } from "semantic-ui-react";
+import { connect } from "react-redux";
 import "semantic-ui-css/semantic.min.css";
 import "./CreatePosts.css";
 
 const CreatePosts = props => {
-  const { handleSubmit, pristine, submitting, users, createPost } = props;
-  const user = users.map(item => (
-    <option key={item.id} value={item.id}>
-      {item.first_name}
-      {item.last_name}
-    </option>
-  ));
-
+  const {
+    handleSubmit,
+    pristine,
+    submitting,
+    users,
+    createPost,
+    userOne
+  } = props;
+  let user;
+  let activeUsers = users;
+  if (activeUsers ? activeUsers : (activeUsers = userOne)) {
+    user = activeUsers.map(item => (
+      <option key={item.id} value={item.id}>
+        {item.first_name}
+        {item.last_name}
+      </option>
+    ));
+  }
   return (
     <>
       <form className="Create-posts" onSubmit={handleSubmit(createPost)}>
         <h2 className="Create-posts__title">Create Post</h2>
-        <div>
-          <label>User</label>
+        {users ? (
           <div>
-            <Field
-              className="Create-posts__user"
-              name="user_id"
-              component="select"
-            >
-              <option />
-              {user}
-            </Field>
+            <label>User</label>
+            <div>
+              <Field
+                className="Create-posts__user"
+                name="user_id"
+                component="select"
+              >
+                <option />
+                {user}
+              </Field>
+            </div>
           </div>
-        </div>
+        ) : null}
 
         <div>
           <label>Title</label>
@@ -60,6 +73,29 @@ const CreatePosts = props => {
   );
 };
 
-export default reduxForm({
-  form: "createPosts" // a unique identifier for this form
-})(CreatePosts);
+// export default reduxForm({
+//   form: "createPosts" // a unique identifier for this form
+// })(CreatePosts);
+
+const mapStateToProps = (state, { userOne, users }) => {
+  let userActualID;
+  console.log(userOne[0]["id"]);
+  if (users) {
+    userActualID = null;
+  } else {
+    userActualID = userOne[0]["id"];
+  }
+
+  return {
+    initialValues: {
+      user_id: userActualID
+    }
+  };
+};
+
+export default connect(mapStateToProps)(
+  reduxForm({
+    form: "createPosts", // a unique identifier for this form
+    enableReinitialize: true
+  })(CreatePosts)
+);
