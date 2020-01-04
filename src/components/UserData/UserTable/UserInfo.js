402044c03e9ac6e3841e4c8261b9575077bcb28b
Darek <dar.lewandowski93@gmail.com>
2020-01-05 00:13:37 +0100

    add class

--- src/components/UserData/UserTable/UserInfo.js
+++ src/components/UserData/UserTable/UserInfo.js
@@ -6,40 +6,39 @@ const UserInfo = props => {
   const { user, createPost, userPost } = props;
 
   const post = userPost.map((item, i) => (
-    <div key={item.id}>
-      <div>Post#{i + 1}</div>
-      <div>{item.title}</div>
-      <div>{item.body}</div>
+    <div className="User_post__content" key={item.id}>
+      <h4 className="User_post__number">Post#{i + 1}</h4>
+      <div className="User_post__title">{item.title}</div>
+      <div className="User_post__body">{item.body}</div>
     </div>
   ));
 
-  const info = (
-    <div>
-      {props.user.map((item, index) => {
-        return (
-          <div key={index}>
-            <p>First Name</p>
-            <p>{item.first_name}</p>
-            <p>Last Name</p>
-            <p>{item.last_name}</p>
-            <p>Email</p>
-            <p>{item.email}</p>
-            <p>Phone</p>
-            <p>{item.phone}</p>
-            <p>Adress</p>
-            <p>{item.address}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+  const info = props.user.map((item, index) => {
+    return (
+      <div className="User_information" key={index}>
+        <p>First Name</p>
+        <p>{item.first_name}</p>
+        <p>Last Name</p>
+        <p>{item.last_name}</p>
+        <p>Email</p>
+        <p>{item.email}</p>
+        <p>Phone</p>
+        <p>{item.phone}</p>
+        <p>Adress</p>
+        <p>{item.address}</p>
+      </div>
+    );
+  });
+
   return (
-    <div>
+    <div className=" User_content">
       {info}
-      {user.length > 0 ? (
-        <CreatePosts userOne={user} createPost={createPost} />
-      ) : null}
-      {post}
+      <div className="User_post-create">
+        {user.length > 0 ? (
+          <CreatePosts userOne={user} createPost={createPost} />
+        ) : null}
+      </div>
+      <div className="User_post">{post}</div>
     </div>
   );
 };
